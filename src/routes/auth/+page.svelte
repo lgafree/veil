a55ca0558c9e9b13b188c8091a1ca47b6176cd51
lgafree <lelix.guile77@gmail.com>
2023-10-01 16:33:55 +0800

    Added a login note

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -4,6 +4,8 @@
 	import { auth } from '$lib/firebase/firebaseConfig.js';
 	import { onMount } from 'svelte';
 
+	let showToast = false;
+
 	onMount(() => {
 		auth.onAuthStateChanged((user) => {
 			if (user) {
@@ -20,8 +22,38 @@
 			}
 		});
 	}
+
+	function copyToClipboard() {
+		navigator.clipboard.writeText(window.location.href);
+		showToast = true;
+
+		setTimeout(() => {
+			showToast = false;
+		}, 5000);
+	}
 </script>
 
-<main class="h-screen grid place-content-center">
-	<Button size="lg" on:click={login}>Log in</Button>
+<main class="h-screen grid place-content-center mx-5">
+	<div>
+		<span class="h-32 mx-auto text-2xl text-primary align-middle"
+			>ATM, log in is only supported in the browser. If you're in an app, please copy the link and
+			paste it in your browser. (e.g, Chrome)</span
+		>
+		<Button variant="outline" size="sm" class="text-primary" on:click={copyToClipboard}
+			>Copy link</Button
+		>
+	</div>
+
+	<div class="width-full items-center mt-5">
+		<Button size="lg" on:click={login}>Log in</Button>
+	</div>
+
+	{#if showToast}
+		<div
+			class="animate-bounce bg-primary w-auto fixed bottom-2 left-2 text-center rounded-sm py-2 px-3"
+		>
+			<span class="text-primary-foreground">Link copied. You can now paste it in your browser.</span
+			>
+		</div>
+	{/if}
 </main>
